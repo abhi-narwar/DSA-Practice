4008531b17ed8460ddcb381e3679d539c9a69cb7
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-11-15 14:31:23 +0530

    added code

---
+++ Leetcode Questions/reverse_words_in_a_string.java
@@ -0,0 +1,15 @@
+class reverse_words_in_a_string {
+    public String reverseWords(String s) {
+        
+        String[] words = s.trim().split("\\s+");
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i > 0) sb.append(" ");
+        }
+        
+        return sb.toString();
+    }
+}
