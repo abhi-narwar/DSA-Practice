20f6d7a46fe74c185bc593e6dde0fa2f7d4a1c24
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-11-26 09:31:02 +0530

    added code

---
+++ Leetcode Questions/pair_sum.java
@@ -0,0 +1,34 @@
+class pair_sum {
+    public int pairSum(ListNode head) {
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        
+        ListNode prev = null;
+        ListNode curr = slow;
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+       
+        int maxSum = 0;
+        ListNode first = head;
+        ListNode second = prev;  
+
+        while (second != null) {
+            maxSum = Math.max(maxSum, first.val + second.val);
+            first = first.next;
+            second = second.next;
+        }
+
+        return maxSum;
+    }
+}
