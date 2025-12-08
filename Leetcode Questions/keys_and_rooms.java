754f86f6e6c55b0b20889b79b5b30d3233e91822
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-12-08 08:26:03 +0530

    added code:

---
+++ Leetcode Questions/keys_and_rooms.java
@@ -0,0 +1,19 @@
+class keys_and_rooms {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        dfs(0, rooms, visited);
+        for (boolean v : visited) {
+            if (!v) return false;
+        }
+        return true;
+    }
+
+    private void dfs(int room, List<List<Integer>> rooms, boolean[] visited) {
+        if (visited[room]) return;
+        visited[room] = true;
+
+        for (int key : rooms.get(room)) {
+            dfs(key, rooms, visited);
+        }
+    }
+}
