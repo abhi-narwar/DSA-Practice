1921a2221170237aa741bc3f17a627621ad02e80
Abhishek narwar <142799395+narwar0042@users.noreply.github.com>
2025-12-13 18:58:04 +0530

    added code

---
+++ Leetcode Questions/contains_duplicate_II.java
@@ -0,0 +1,13 @@
+class contains_duplicate_II {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+}
